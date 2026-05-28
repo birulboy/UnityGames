ba4509d404c355141d2d5beb359f5851b690694f
birulboy <brahian.castro@utp.edu.co>
2026-05-27 23:53:56 -0500

    Levelone terminado

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using UnityEngine;
 using System.Collections.Generic;
+
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 4;
@@ -11,7 +12,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float jumpForce = 3;
     private Animator anim;
     private bool isAttacking = false;
-    public bool isStunned = false; // agregado
+    public bool isStunned = false;
 
     [SerializeField] private Attack1HitBox attack1Hitbox;
 
@@ -29,62 +30,52 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        
-
         if (!isAttacking && !isStunned)
-            {
-                float input = Input.GetAxisRaw("Horizontal");
-                rb.linearVelocity = new Vector2(input * moveSpeed, rb.linearVelocity.y);
-                anim.SetInteger("speed", (int)input);
-                if (input > 0) transform.localScale = new Vector3(Mathf.Abs(originalScale.x), originalScale.y, originalScale.z);
-                else if (input < 0) transform.localScale = new Vector3(-Mathf.Abs(originalScale.x), originalScale.y, originalScale.z);
-            }
-        //salto
+        {
+            float input = Input.GetAxisRaw("Horizontal");
+            rb.linearVelocity = new Vector2(input * moveSpeed, rb.linearVelocity.y);
+            anim.SetInteger("speed", (int)input);
+            if (input > 0) transform.localScale = new Vector3(Mathf.Abs(originalScale.x), originalScale.y, originalScale.z);
+            else if (input < 0) transform.localScale = new Vector3(-Mathf.Abs(originalScale.x), originalScale.y, originalScale.z);
+        }
+
         if (Input.GetButtonDown("Jump") && IsGrounded)
-            {
-                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-                anim.SetInteger("height", 1);
-            }
-        //ataque 1
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            anim.SetInteger("height", 1);
+        }
+
         if (Input.GetKeyDown(KeyCode.J) && !isAttacking)
-            {
-                isAttacking = true;
-                attack1Hitbox.EnableHitbox();
-                rb.linearVelocity = new Vector2(0, rb.linearVelocity.y);
-                anim.SetInteger("speed", 0);
-                anim.SetTrigger("attack1");
-                StartCoroutine(ResetAttack1());
-            }
-        //ataque2
-        // if (Input.GetKeyDown(KeyCode.K) && !isAttacking)
-        //     {
-        //         isAttacking = true;
-        //         rb.linearVelocity = new Vector2(0, rb.linearVelocity.y);
-        //         anim.SetInteger("speed", 0);
-        //         anim.SetTrigger("attack2");
-        //         StartCoroutine(ResetAttack2());
-        //     }
+        {
+            isAttacking = true;
+            attack1Hitbox.EnableHitbox();
+            rb.linearVelocity = new Vector2(0, rb.linearVelocity.y);
+            anim.SetInteger("speed", 0);
+            anim.SetTrigger("attack1");
+            StartCoroutine(ResetAttack1());
+        }
     }
 
     IEnumerator ResetAttack1()
-        {
-            yield return new WaitForSeconds(0.214f);
-            attack1Hitbox.DisableHitbox();
-            isAttacking = false;
-        }
+    {
+        yield return new WaitForSeconds(0.214f);
+        attack1Hitbox.DisableHitbox();
+        isAttacking = false;
+    }
+
     IEnumerator ResetAttack2()
-        {
-            yield return new WaitForSeconds(0.700f);
-            isAttacking = false;
-        }
+    {
+        yield return new WaitForSeconds(0.700f);
+        isAttacking = false;
+    }
 
     private void OnCollisionEnter2D(Collision2D other)
-    {   
+    {
         if (other.gameObject.CompareTag("Ground"))
         {
             IsGrounded = true;
             anim.SetInteger("height", 0);
-        }    
+        }
     }
 
     private void OnCollisionExit2D(Collision2D other)
