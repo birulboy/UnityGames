52e17784bf490da128f118638a6195635da7cec6
JulRiv07 <Julian.rivera@utp.edu.co>
2026-06-05 00:23:41 -0500

    Resolver Attack1HitBox

--- Assets/Scripts/Attack1HitBox.cs
+++ Assets/Scripts/Attack1HitBox.cs
@@ -3,21 +3,15 @@ using UnityEngine;
 public class Attack1HitBox : MonoBehaviour
 {
     public int damage = 10;
-<<<<<<< HEAD
-
-=======
     public bool isAttack2 = false;
->>>>>>> abfe308ed42e873855e52b5d885d8fe605b3f389
+
     private bool isAttacking = false;
     private bool hasHit = false;
 
     public void EnableHitbox()
     {
         isAttacking = true;
-<<<<<<< HEAD
-=======
         hasHit = false;
->>>>>>> abfe308ed42e873855e52b5d885d8fe605b3f389
     }
 
     public void DisableHitbox()
@@ -26,26 +20,17 @@ public class Attack1HitBox : MonoBehaviour
         hasHit = false;
     }
 
-<<<<<<< HEAD
     private void OnTriggerStay2D(Collider2D other)
     {
-        if (!isAttacking)
+        if (!isAttacking || hasHit)
             return;
 
-        // Enemigos normales
-        EnemyHealth enemy = other.GetComponentInParent<EnemyHealth>();
+        ApplyDamage(other);
+    }
 
-        if (enemy != null)
-        {
-            enemy.TakeDamage(damage);
-
-            Debug.Log("Golpeé un enemigo");
-
-            isAttacking = false;
-            return;
-        }
-
-        // Boss
+    private void ApplyDamage(Collider2D other)
+    {
+        // BOSS
         BossHealth boss = other.GetComponentInParent<BossHealth>();
 
         if (boss != null)
@@ -54,31 +39,34 @@ public class Attack1HitBox : MonoBehaviour
 
             Debug.Log("Golpeé al BOSS");
 
-            isAttacking = false;
-            return;
-=======
-    void OnTriggerStay2D(Collider2D other)
-    {
-        if (isAttacking && !hasHit && other.CompareTag("Enemy"))
-            ApplyDamage(other);
-    }
-
-    private void ApplyDamage(Collider2D other)
-    {
-        EnemyHealth enemy = other.GetComponent<EnemyHealth>();
-        if (enemy != null)
-        {
-            enemy.TakeDamage(damage);
             hasHit = true;
             return;
         }
 
-        SkeletonHealth skeleton = other.GetComponent<SkeletonHealth>();
+        // ENEMIGO NORMAL
+        EnemyHealth enemy = other.GetComponentInParent<EnemyHealth>();
+
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+
+            Debug.Log("Golpeé un enemigo");
+
+            hasHit = true;
+            return;
+        }
+
+        // SKELETON
+        SkeletonHealth skeleton = other.GetComponentInParent<SkeletonHealth>();
+
         if (skeleton != null)
         {
             skeleton.TakeDamage(damage, isAttack2);
+
+            Debug.Log("Golpeé un Skeleton");
+
             hasHit = true;
->>>>>>> abfe308ed42e873855e52b5d885d8fe605b3f389
+            return;
         }
     }
 }
